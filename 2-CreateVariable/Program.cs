b8af1209daf635edb6844ea94067aa749748e80f
Erick Ribeiro <gustavoribeirerick@gmail.com>
2023-04-05 12:14:45 -0300

    crate variable

---
+++ 2-CreateVariable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 2 - Criando Variáveis");
+
+        int age;
+        age = 27;
+
+        Console.WriteLine("sua idade é: " + age);
+
+        Console.Write("teste console write");
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
